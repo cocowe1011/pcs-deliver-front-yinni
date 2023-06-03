8681e55dda803a5d697eea523ed20f6428c0510d
cocowe1011 <wenliang@msunsoft.com>
2023-06-04 03:36:27 +0800

    xiugai

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -3,8 +3,8 @@
     <div class="maskDiv">
       <div class="maskDiv-top">
         <div class="maskDiv-top-left">
-          <img src="../../../build/icons/64x64.png" style="width:45px;height:45px;" />
-          <div>
+          <img src="../../../build/icons/64x64.png" style="width:38px;height:38px;" />
+          <div style="margin-left:10px;height:100%;">
             <div class="maskDiv-top-left-top-title">全自动束下输送系统</div>
             <div class="maskDiv-top-left-top-title2">ccs</div>
           </div>
@@ -36,9 +36,82 @@
         <div class="maskDiv-top-close" @click="closewindow"><i class="el-icon-close" style="font-size:18px;font-weight:600;"></i></div>
       </div>
       <div class="maskDiv-down">
-        <router-view /></div>
+        <div class="maskDiv-down-left">
+          <div class="maskDiv-down-left-top">
+            <div>
+              <div class="card-title">实时数据监控</div>
+              <div class="card-content">
+                <div class="data-card" style="padding:14px 10px 7px 20px">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">箱子长度</div>
+                    <div class="data-card-border-borderDown">500mm</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 14px 10px 7px 10px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">箱子宽度</div>
+                    <div class="data-card-border-borderDown">200mm</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 14px 20px 7px 10px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">箱子高度</div>
+                    <div class="data-card-border-borderDown">200mm</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 7px 10px 7px 20px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">箱子重量</div>
+                    <div class="data-card-border-borderDown">2.3kg</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 7px 10px 7px 10px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">束下设置速度</div>
+                    <div class="data-card-border-borderDown">1.6米/分钟</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 7px 20px 7px 10px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop granient-text">束下实时速度</div>
+                    <div class="data-card-border-borderDown">0.0米/分钟</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 7px 10px 14px 20px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop granient-text">运行系数</div>
+                    <div class="data-card-border-borderDown">k25=0.00</div>
+                  </div>
+                </div>
+                <div class="data-card" style="padding: 7px 10px 14px 10px;">
+                  <div class="data-card-border">
+                    <div class="data-card-border-borderTop">订单圈数</div>
+                    <div class="data-card-border-borderDown">0</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="maskDiv-down-left-down">
+            <div>
+              <div class="card-title">操作日志</div>
+              <div class="card-content"></div>
+            </div>
+          </div>
+        </div>
+        <div class="maskDiv-down-right">
+          <div>
+            <div class="card-title">实时状态监控</div>
+            <div class="card-content chuansongpadding">
+              <img src="./img/fushe2x.png" class="fusheIcon"/>
+              <img src="./img/deng.png" class="fusheguang"/>
+              <img src="./img/chuansongdai.png" style="width:100%;height:100%" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -129,7 +202,26 @@ export default {
         align-items: center;
         padding-left: 10px;
         &-top-title {
-          
+          font-size: 16px;
+          font-weight: 550;
+          letter-spacing: 0px;
+          color: rgba(28, 28, 40, 1);
+          text-align: left;
+          vertical-align: top;
+          height: 30px;
+          display: flex;
+          align-items: flex-end;
+        }
+        &-top-title2 {
+          font-size: 14px;
+          font-weight: 400;
+          letter-spacing: 1.2px;
+          color: rgba(70, 91, 101, 1);
+          text-align: left;
+          vertical-align: top;
+          height: 20px;
+          display: flex;
+          align-items: flex-start;
         }
       }
       &-min,&-close,&-max {
@@ -194,6 +286,134 @@ export default {
     &-down {
       width: 100%;
       height: calc(100% - 55px);
+      .card-title {
+        height: 30px;
+        width: 100%;
+        font-weight: 550;
+        letter-spacing: 0px;
+        color: rgba(0, 0, 0, 1);
+        text-align: left;
+        vertical-align: top;
+        display: flex;
+        align-items: flex-end;
+        padding-left: 18px;
+      }
+      .card-content {
+        height: calc(100% - 37px);
+        width: 100%;
+        .data-card {
+          width: 33.3333%;
+          height: 33.3333%;
+          float: left;
+          box-sizing: border-box;
+          padding: 5px 10px 5px 10px;
+          &-border {
+            width: 100%;
+            height: 100%;
+            border-radius: 20px;
+            background: rgba(40, 37, 59, 1);
+            box-shadow: 0px 20px 50px 0px rgba(0, 0, 0, 0.25);
+            &-borderTop {
+              font-weight: 400;
+              letter-spacing: 0px;
+              color: rgba(189, 189, 189, 1);
+              text-align: left;
+              vertical-align: top;
+              font-size: 14px;
+              line-height: 38px;
+              padding-left: 14px;
+            }
+            .granient-text {
+              background-image: linear-gradient(to right, rgba(72, 146, 254, 1) , rgba(71, 207, 245, 1));
+              background-clip: text;
+              -webkit-background-clip: text;
+              color: transparent;
+            }
+            &-borderDown {
+              font-weight: 700;
+              letter-spacing: 0px;
+              color: rgba(255, 255, 255, 1);
+              text-align: left;
+              vertical-align: top;
+              font-size: 28px;
+              line-height: 32px;
+              padding-left: 14px;
+            }
+          }
+        }
+      }
+      .fusheIcon {
+        width:45px;
+        height:45px;
+        position: absolute;
+        left: 430px;
+        top: 7px;
+      }
+      .fusheguang {
+        width: 126px;
+        height: 140px;
+        position: absolute;
+        top: 53px;
+        left: 389px;
+      }
+      .chuansongpadding {
+        box-sizing: border-box;
+        padding: 100px 70px 62px 70px;
+      }
+      &-left{
+        width: calc(100% - 930px);
+        height: 100%;
+        float: left;
+        &-top {
+          height: 50%;
+          width: 100%;
+          float: left;
+          box-sizing: border-box;
+          padding: 15px 15px 7.5px 15px;
+          > div {
+            width: 100%;
+            height: 100%;
+            opacity: 1;
+            border-radius: 20px;
+            background: rgba(246, 247, 251, 0.56);
+            box-shadow: 0px 60px 90px 0px rgba(0, 0, 0, 0.2);
+            backdrop-filter: blur(88px);
+          }
+        }
+        &-down {
+          height: 50%;
+          width: 100%;
+          float: left;
+          box-sizing: border-box;
+          padding: 7.5px 15px 15px 15px;
+          > div {
+            width: 100%;
+            height: 100%;
+            opacity: 1;
+            border-radius: 20px;
+            background: rgba(246, 247, 251, 0.56);
+            box-shadow: 0px 60px 90px 0px rgba(0, 0, 0, 0.2);
+            backdrop-filter: blur(88px);
+          }
+        }
+      }
+      &-right{
+        width: 930px;
+        height: 100%;
+        float: left;
+        padding: 15px 15px 15px 0px;
+        box-sizing: border-box;
+        > div {
+          width: 100%;
+          height: 100%;
+          opacity: 1;
+          border-radius: 20px;
+          background: rgba(246, 247, 251, 0.56);
+          box-shadow: 0px 60px 90px 0px rgba(0, 0, 0, 0.2);
+          backdrop-filter: blur(88px);
+          background: linear-gradient(to right, rgba(83, 188, 206, 0.7), rgba(97, 168, 160, 0.8));
+        }
+      }
     }
   }
 }
